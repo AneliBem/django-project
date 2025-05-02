b78a40e0f600c2326555844dc4487ff1d1e4fabc
AneliBem <tolya.bem@gmail.com>
2025-05-02 22:47:12 +0300

    finish register models in admin

--- main/models.py
+++ main/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 # клас з розмірною сіткою, 10 символів та розміри унікальні. __str__ для правильного відображення
 class Size(models.Model):
     name = models.CharField(max_length=10, unique=True)
 
     def __str__(self):
         return self.name
+
 
 # клас категорії, 255 символів та моделі унікальні, Створює таблицю в базі даних з полями: id name slug; 
 # Meta сортування по імені, призначається індекс для поля name, verbose_name та verbose_name_plural визначають, як модель називатиметься в адмін-панелі 
@@ -24,4 +26,38 @@ class Category(models.Model):
 
 
 class ClothingItem(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    available = models.BooleanField(default=True)
+    sizes = models.ManyToManyField(Size, through='ClothingItemSize',
+                                   related_name='clothing_item', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                 related_name='clothing_items')   
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    discount = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+    def __str__(self):
+        return self.name
     
+
+    def get_price_with_discount(self):
+        if self.discount > 0:
+            return self.price * (1 - (self.discount / 100))
+        return self.price
+    
+
+class ClothingItemSize(models.Model):
+    clothing_item = models.ForeignKey(ClothingItem, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    available = models.BooleanField(default=True)
+
+
+    class Meta:
+        unique_together = ('clothing_item', 'size')
+
+
+
